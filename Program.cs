f5966adbf7e23eb558bf90d63c9de23331ef36a3
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-12-06 18:36:51 -0300

    programa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 1; i <= 40; i++)
+     
+        {
+            // Verifica se o número atual é múltiplo de 4.
+            if (i % 4 == 0)
+            {
+                Console.Write("pim");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+ 
+            // Adiciona um espaço após cada número ou palavra, exceto no final da linha.
+            if (i % 10 != 0)
+            {
+                Console.Write(" ");
+            }
+            else
+            {
+                // Quebra a linha após 10 números/palavras.
+                Console.WriteLine();
+            }
+        }
